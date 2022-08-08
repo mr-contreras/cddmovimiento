689592a228f25c58166564426ea4d0bced6042f6
Eligio Andrés Chan España <102203006+eacespania@users.noreply.github.com>
2022-08-08 03:43:30 -0500

    Sales order minimum quantity
    Sales order minimum quantity

---
+++ do_sale_product_minimum_quantity/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Sale Order Product Minimum Quantity',
+    'summary': """
+        This modules support for Sale Order Product Minimum Quantity.
+    """,
+    'description': """
+        This modules support for Sale Order Product Minimum Quantity.
+    """,
+    'author': 'Do Incredible',
+    'website': 'http://doincredible.com',
+    'license': 'OPL-1',
+    'category': 'Product',
+    'sequence': '10',
+    'version': '1.0.0',
+    'depends': ['sale_management', 'do_product_minimum_quantity'],
+    'data': [
+    ],
+    'images': ['static/description/main.png'],
+    'application': True,
+    'live_test_url': '',
+    'price': 5,
+    'currency': 'USD',
+}
---
+++ do_sale_product_minimum_quantity/models/sale_order.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, models, _
+from odoo.exceptions import ValidationError
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    @api.constrains('order_line')
+    def check_constraint_quantity(self):
+        for record in self:
+            if record.order_line:
+                for line in record.order_line:
+                    minimum_order_qty = line.product_id.minimum_quantity
+                    if line.product_uom_qty < minimum_order_qty:
+                        raise ValidationError(_('Minimum order quantity of the product ' + line.name + ' is ' + str(minimum_order_qty)))
