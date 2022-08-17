756ab91d428202f2ebd89d996320773368910560
Eligio Andrés Chan España <102203006+eacespania@users.noreply.github.com>
2022-08-17 05:42:55 -0500

    Update sale_order.py
    Validar un check para en algunos casos apagar los minimos por producto

--- do_sale_product_minimum_quantity/models/sale_order.py
+++ do_sale_product_minimum_quantity/models/sale_order.py
@@ -13,5 +13,5 @@ class SaleOrder(models.Model):
             if record.order_line:
                 for line in record.order_line:
                     minimum_order_qty = line.product_id.minimum_quantity
-                    if line.product_uom_qty != minimum_order_qty:
+                    if line.product_uom_qty != minimum_order_qty and line.product_id.x_studio_limite_habilitado:
                         raise ValidationError(_('Este producto/servicio ' + line.name + ' solamente puede venderse en ' + str(minimum_order_qty) + ' unidades.'))
