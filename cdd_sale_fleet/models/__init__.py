5312cb749dc53742a72d078e62afe30a9855fe91
Eligio Chan <echan@ezzquad.com>
2023-04-09 13:30:43 -0600

    Update __init__.py

--- cdd_sale_fleet/models/__init__.py
+++ cdd_sale_fleet/models/__init__.py
@@ -4,3 +4,6 @@ from . import hr_employee
 from . import project
 from . import sale_order
 from . import account_move
+from . import fleet_services_config
+from . import fleet_services_notifications
+from . import fleet_vehicle_log_service_inh
